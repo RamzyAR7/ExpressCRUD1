bcef5baae3c715c029e248a23598582f99f3c609
RamzyAR7 <rameya683@gmail.com>
2024-12-12 02:27:22 +0200

    add all

--- Server.js
+++ Server.js
@@ -3,29 +3,34 @@ const mongoose = require('mongoose')
 const UsersRoutes = require('./routes/Users.Routes')
 require('dotenv').config()
 
+//app obj new app
 const app = express();
-
-// middlware
-app.use(express.json());
-app.use(express.urlencoded({extended: false}))
 
 //routes
 app.use("/users", UsersRoutes);
 
-
+//test
 app.get('/', (req, res) => {
     res.send("hellooooo")
 })
+
+//server and DB
 const PORT = 3000;
 const DB_URL = process.env.DB;
 mongoose.connect(DB_URL).then(() => {
     app.listen(PORT, () => {
         console.log("start")
         console.log("db connected")
-        console.log("http://localhost:3000")
+        console.log(`http://localhost:${PORT}`)
     });
 }).catch((error) => {
     console.log(error)
 })
 
-module.exports = app;
+
+// npm init
+// npm install express
+// npm i nodemon
+// npm install mongodb
+// npm install mongoose
+// npm install bcrypt //auth
